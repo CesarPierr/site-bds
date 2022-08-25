f4c471ddf3e7ace8636e71ed98b5c90db16e4944
Pierre <pierre.cesar38@gmail.com>
2022-08-25 15:08:51 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { SlidesComponent } from './slides/slides.component';
+import { FrontpageComponent } from './frontpage/frontpage.component';
+import { CardListComponent } from './card-list/card-list.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    SlidesComponent,
+    FrontpageComponent,
+    CardListComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
